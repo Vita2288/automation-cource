db134db4c68d003d68308627de93539b02c3872d
Vita Kucheruk <67745350+Vita2288@users.noreply.github.com>
2021-05-25 10:14:08 +0300

    add log4j

--- src/test/java/vkucheruk/secondLesson/GooglePageTests.java
+++ src/test/java/vkucheruk/secondLesson/GooglePageTests.java
@@ -6,9 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 public class GooglePageTests {
-    WebDriver driver = new ChromeDriver();
- /*   System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    driver.manage().window().maximaze();
-    driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-    driver.get("https://passport.yandex.ru/auth");*/
+    public static void main(String[] args) {
+
+
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com.ua/");
+    }
 }
