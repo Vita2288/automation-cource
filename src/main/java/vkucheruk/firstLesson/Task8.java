8254b2ce57f585a6b23e5f06fcccfeae81d1e281
Vita Kucheruk <67745350+Vita2288@users.noreply.github.com>
2021-05-29 10:57:19 +0300

    fix task 8

--- src/main/java/vkucheruk/firstLesson/Task8.java
+++ src/main/java/vkucheruk/firstLesson/Task8.java
@@ -2,41 +2,44 @@ package vkucheruk.firstLesson;
 
 public class Task8 {
     public static void main(String[] args) {
-        checkSeason(12);
-        checkSeason(4);
+        checkSeason(13);
+        checkSeason(0);
         checkSeason(7);
-        checkSeason(10);
+        checkSeason(9);
     }
 
     public static void checkSeason(int month) {
-        switch (month){
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("winter");
-                break;
-
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("spring");
-                break;
-
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("summer");
-                break;
-
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("fall");
-                break;
-
-            default:
-                System.out.println("Wrong number of month: " + month);
+        if (month < 1 || month > 12) {
+            System.out.println("Wrong number of month: " + month);
+            return;
         }
+
+        switch (month) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("winter");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("spring");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("summer");
+                    break;
+
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("fall");
+                    break;
+        }
+
 
     }
 }
